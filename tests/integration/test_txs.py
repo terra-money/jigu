065db7cb2f156be5de247fc05f3828db679d17dd
William Chen <wchen298@gmail.com>
2020-03-09 08:49:52 +0900

    uncomment tests

--- tests/integration/test_txs.py
+++ tests/integration/test_txs.py
@@ -50,74 +50,74 @@ class TestTx:
         err = excinfo.value
         assert err.codespace == "oracle"
 
-    # def test_make_proposal(self, wallet, fee):
-    #     e = MsgSubmitProposal(
-    #         content=ParameterChangeProposal(
-    #             "testing params",
-    #             "yay!",
-    #             changes={
-    #                 "distribution": {
-    #                     "community_tax": Dec(0),
-    #                     "base_proposer_reward": Dec(32),
-    #                     "bonus_proposer_reward": Dec(22),
-    #                     "withdraw_addr_enabled": True,
-    #                 },
-    #                 "staking": {
-    #                     "unbonding_time": 33,
-    #                     "max_validators": 9999,
-    #                     "max_entries": 2323,
-    #                     "bond_denom": "uluna",
-    #                 },
-    #                 "slashing": {
-    #                     "max_evidence_age": 234234,
-    #                     "signed_blocks_window": 1,
-    #                     "min_signed_per_window": Dec(1),
-    #                     "downtime_jail_duration": 1,
-    #                     "SlashFractionDoubleSign": Dec(100),
-    #                     "slash_fraction_downtime": Dec(213.123),
-    #                 },
-    #                 "treasury": {
-    #                     "tax_policy": PolicyConstraints(
-    #                         rate_min=Dec(0),
-    #                         rate_max=Dec(100),
-    #                         cap=Coin("unused", 0),
-    #                         change_max=Dec(3),
-    #                     ),
-    #                     "reward_policy": PolicyConstraints(
-    #                         rate_min=Dec(0),
-    #                         rate_max=Dec(1023423340),
-    #                         cap=Coin("unused", 0),
-    #                         change_max=Dec(3),
-    #                     ),
-    #                     "seigniorage_burden_target": Dec("2342.234234"),
-    #                     "mining_increment": Dec(23423423423.234234234234982),
-    #                     "window_short": 50,
-    #                     "window_long": 2,
-    #                     "window_probation": 30,
-    #                 },
-    #                 "oracle": {
-    #                     "vote_period": 345345,
-    #                     "vote_threshold": Dec("2342.234333"),
-    #                     "reward_band": Dec("234343"),
-    #                     "reward_distribution_window": 345345,
-    #                     "whitelist": ["abc", "bdc", "ttt"],
-    #                     "slash_fraction": Dec(23423.232343),
-    #                     "slash_window": 343311,
-    #                     "min_valid_per_window": Dec(2342.234234),
-    #                 },
-    #                 "market": {
-    #                     "pool_recovery_period": 234234234,
-    #                     "base_pool": Dec(232323232),
-    #                     "min_spread": Dec(343434),
-    #                     "illiquid_tobin_tax_list": [{"denom": "item", "msg": "sdfsdf"}],
-    #                 },
-    #             },
-    #         ),
-    #         initial_deposit=Coins(uluna=10000000),
-    #         proposer=wallet.address,
-    #     )
-    #     print(wallet.address)
-    #     print(wallet.account_number, wallet.sequence)
-    #     tx = wallet.create_and_sign_tx(e, fee=fee)
-    #     res = wallet.broadcast(tx)
+    def test_make_proposal(self, wallet, fee):
+        e = MsgSubmitProposal(
+            content=ParameterChangeProposal(
+                "testing params",
+                "yay!",
+                changes={
+                    "distribution": {
+                        "community_tax": Dec(0),
+                        "base_proposer_reward": Dec(32),
+                        "bonus_proposer_reward": Dec(22),
+                        "withdraw_addr_enabled": True,
+                    },
+                    "staking": {
+                        "unbonding_time": 33,
+                        "max_validators": 9999,
+                        "max_entries": 2323,
+                        "bond_denom": "uluna",
+                    },
+                    "slashing": {
+                        "max_evidence_age": 234234,
+                        "signed_blocks_window": 1,
+                        "min_signed_per_window": Dec(1),
+                        "downtime_jail_duration": 1,
+                        "SlashFractionDoubleSign": Dec(100),
+                        "slash_fraction_downtime": Dec(213.123),
+                    },
+                    "treasury": {
+                        "tax_policy": PolicyConstraints(
+                            rate_min=Dec(0),
+                            rate_max=Dec(100),
+                            cap=Coin("unused", 0),
+                            change_max=Dec(3),
+                        ),
+                        "reward_policy": PolicyConstraints(
+                            rate_min=Dec(0),
+                            rate_max=Dec(1023423340),
+                            cap=Coin("unused", 0),
+                            change_max=Dec(3),
+                        ),
+                        "seigniorage_burden_target": Dec("2342.234234"),
+                        "mining_increment": Dec(23423423423.234234234234982),
+                        "window_short": 50,
+                        "window_long": 2,
+                        "window_probation": 30,
+                    },
+                    "oracle": {
+                        "vote_period": 345345,
+                        "vote_threshold": Dec("2342.234333"),
+                        "reward_band": Dec("234343"),
+                        "reward_distribution_window": 345345,
+                        "whitelist": ["abc", "bdc", "ttt"],
+                        "slash_fraction": Dec(23423.232343),
+                        "slash_window": 343311,
+                        "min_valid_per_window": Dec(2342.234234),
+                    },
+                    "market": {
+                        "pool_recovery_period": 234234234,
+                        "base_pool": Dec(232323232),
+                        "min_spread": Dec(343434),
+                        "illiquid_tobin_tax_list": [{"denom": "item", "msg": "sdfsdf"}],
+                    },
+                },
+            ),
+            initial_deposit=Coins(uluna=10000000),
+            proposer=wallet.address,
+        )
+        print(wallet.address)
+        print(wallet.account_number, wallet.sequence)
+        tx = wallet.create_and_sign_tx(e, fee=fee)
+        res = wallet.broadcast(tx)
 
